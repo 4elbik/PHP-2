5ed6226e46b5060d837e479b1417581be44ae974
4elbik <4elbik@gmail.com>
2016-08-17 03:05:08 +0300

    Удаление оставшихся и ненужных функций

--- functions/sql.php
+++ functions/sql.php
@@ -31,27 +31,3 @@ class Use_db {
         mysql_query($query);
     }
 }
-
-
-function connect_db()
-{
-    mysql_connect('localhost', 'root', '');
-    mysql_select_db('php_2');
-}
-
-function sql_query($query)
-{
-    connect_db();
-    $res = mysql_query($query);
-
-    while (false !== $row = mysql_fetch_assoc($res)) {
-        $ret[] = $row;
-    }
-    return $ret;
-}
-
-function sql_exec($query)
-{
-    connect_db();
-    $res = mysql_query($query);
-}
